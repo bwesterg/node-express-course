12c4a385043b4a236d0b312b19be2d3a2461f290
bwesterg <benton.westergaard@gmail.com>
2024-01-24 14:55:38 -0700

    adds private connection variable

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,9 @@
 // console.log('Task Manager App test');
-require('./db/connect')
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middleware
 app.use(express.json())
@@ -12,7 +13,7 @@ app.get('/hello', (req, res)=> {
     res.send('Task Manager App')
 })
 
-app.use('/api/v1/tasks',tasks)
+app.use('/api/v1/tasks', tasks)
 
 //https://hn.algolia.com/api
 // app.get('/api/v1/tasks')         - get all the tasks
@@ -23,5 +24,13 @@ app.use('/api/v1/tasks',tasks)
 
 const port = 3000;
 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+start()
